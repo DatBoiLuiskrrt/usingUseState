b7cc866a1f82189d7c07cdf11e418cbc88687716
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-11 11:46:21 -0500

    Mainly working on setting up the form which will display the api data

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
-
+const api = {
+  key: "00e191444f8db177744da31dfc1ae87e",
+  url: "https://api.openweathermap.org/data/2.5/",
+}
 
 function App() {
-  const [ count, setCount ] = useState(0);
-
-  const increment = () => {
-    return setCount( count + 1 );
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let day = days.[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMoth()];
+    let year = d.getFullYear();
+    
+    return `${day} ${date} ${month} ${year}`
   }
-
-  const decrement = () => {
-    return setCount( count - 1 );
-  }
-
+  
   return (
-    <div>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
+    <div className="app">
+     <main>
+       <div className="search-box">
+         <input
+         type="text"
+         className="search-bar"
+         placeholder="Search..."
+         />
+         <div className="location-box">
+           <div className="location">New York City, US</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+         </div>
+       </div>
+     </main>
     </div>
   );
 }
